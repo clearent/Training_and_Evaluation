62db0fc5b707a90ccbf912c32892bcd52f9d9709
Shirmond Conway <shirmond.conway@gmail.com>
2013-12-02 21:28:59 -0600

    Add newly created classes and unit tests

---
+++ ProjectEuler2/SolveProjectEuler2/FibonacciSequencer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveProjectEuler2
+{
+    public class FibonacciSequencer : IFibonacciSequencer
+    {
+        public IList<long> GenerateFibonacciSequence(long first, long second, long max, IList<long> l)
+        {
+            var x = first + second;
+            if (x <= max)
+            {
+                l.Add(x);
+                l = GenerateFibonacciSequence(second, x, max, l);
+            }
+
+            return l;
+        }
+    }
+}
